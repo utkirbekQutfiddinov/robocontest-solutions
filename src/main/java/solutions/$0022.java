a2a191b756d873f50c115a1703d43b17e579a78b
utkirbek <utkirbek_qutfiddinov@mail.ru>
2021-12-27 13:02:39 +0500

    bla bla bla

---
+++ src/main/java/solutions/$0022.java
@@ -0,0 +1,11 @@
+package solutions;
+
+import java.util.Scanner;
+
+public class $0022 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(n/10);
+    }
+}
