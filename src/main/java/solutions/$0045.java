f262944b03e55ba4ddc13f7b22d398eb3c1358db
utkirbek <utkirbek_qutfiddinov@mail.ru>
2022-01-08 04:43:31 +0500

    0045

---
+++ src/main/java/solutions/$0045.java
@@ -0,0 +1,12 @@
+package solutions;
+
+import java.util.Scanner;
+
+public class $0045 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        long res=n*(n+1)/2;
+        System.out.println(res);
+    }
+}
